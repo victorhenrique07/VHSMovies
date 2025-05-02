d9ae9d4726d8fa1b130c5910a8eaf257db2e2f41
Victor Henrique Silva <henrictor@hotmail.com>
2025-05-02 15:09:51 -0300

    .

--- tests/VHSMovies.Tests/obj/Debug/net9.0/VHSMovies.Tests.AssemblyInfo.cs
+++ tests/VHSMovies.Tests/obj/Debug/net9.0/VHSMovies.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VHSMovies.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b873278b2caf9b1f9e5b917f0b967da63e1058c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bbdca63fb448d05047029431e07de6ee028bfac")]
 [assembly: System.Reflection.AssemblyProductAttribute("VHSMovies.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VHSMovies.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
