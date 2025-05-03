f33ce6927392a649e2cd193b82901f8215685ecf
Victor Henrique Silva <henrictor@hotmail.com>
2025-05-03 14:32:50 -0300

    .

--- src/VHSMovies.Website/Settings/Configuration.cs
+++ src/VHSMovies.Website/Settings/Configuration.cs
@@ -3,6 +3,6 @@
     public static class Configuration
     {
         public static string HttpClientName = "vhsmovies";
-        public static string BackendUrl = Environment.GetEnvironmentVariable("API_URL");
+        public static string BackendUrl = "https://api.vhsmovies.com.br";
     }
 }
