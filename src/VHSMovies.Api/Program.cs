b0e660060b3bc6f23a9d29d08488da968889149e
Victor Henrique Silva <henrictor@hotmail.com>
2025-05-03 16:40:59 -0300

    .

--- src/VHSMovies.Api/Program.cs
+++ src/VHSMovies.Api/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-builder.WebHost.UseUrls($"https://*:{port}");
+builder.WebHost.UseUrls($"http://*:{port}");
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
